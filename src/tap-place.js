595079590b68cc6e6ff6d3158ee2d271b7475e6d
elegazzo <sixelafr20@gmail.com>
2026-06-30 01:35:08 -0600

    actualizacion

--- src/tap-place.js
+++ src/tap-place.js
@@ -50,10 +50,12 @@ export const tapPlaceComponent = {
 
     ground.addEventListener('click', (event) => {
       if (!this.canPlaceModel) return
+      this.prompt.style.cssText =
+        'font-size:16px !important; display:block !important; position:static !important; transform:none !important; margin-top:6px;'
 
-      this.prompt.style.display = 'block'
-      this.prompt.innerHTML ='👆👆 Doble tap sobre un nudo para abrir su memoria.'
-      this.canPlaceModel = false
+      this.prompt.innerHTML =
+        '👆👆 Doble tap sobre un nudo para abrir su memoria.'
+            this.canPlaceModel = false
       let nodeId
 
       if (this.mode === 'preset') {
